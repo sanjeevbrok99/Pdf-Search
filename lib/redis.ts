864288f5813b8812822d6cf5e43766407a2984c8
sanjeevbrok99 <mishragsanjeev1999@gmail.com>
2025-04-27 18:05:08 +0530

    added redis url from upstash

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,8 +1,10 @@
 import Redis from 'ioredis';
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST || 'localhost', // Redis host
-  port: parseInt(process.env.REDIS_PORT || '6379', 10), // Redis port
-});
+export const redis = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL, { tls: {} })
+  : new Redis({
+      host: '127.0.0.1', // Localhost Redis
+      port: 6379,
+    });
 
 export default redis;
